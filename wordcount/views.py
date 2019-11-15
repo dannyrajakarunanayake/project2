93f0cc638e1ec9ce3043b33ebeba7dbdfc5e9ab2
dannyraj <mesh.ferdi@gmail.com>
2019-11-15 11:26:25 +1100

    commit

---
+++ wordcount/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+
+def homepage(request):
+    return render(request,'home.html')
+
+def count(request):
+    return render(request,'count.html')
+
+def aboutpage(request):
+    return render(request, 'about.html')    
